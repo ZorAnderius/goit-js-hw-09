58600e71712abb2e4ea4f40ae14ec174ac1f4d5d
ZorAnderius <119595649+ZorAnderius@users.noreply.github.com>
2023-05-14 13:48:34 +0000

    Deploying to gh-pages from @ ZorAnderius/goit-js-hw-09@bf72e7806cea3df4edf3c78bb2a9c07261f28aa4 🚀

--- 01-color-switcher.cd0062f3.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;t.addEventListener("click",(function(){a=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,t.classList.add("isNotActive")})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,t.classList.remove("isNotActive")}));
-//# sourceMappingURL=01-color-switcher.cd0062f3.js.map
